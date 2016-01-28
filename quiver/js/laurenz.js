c11984384e8126ece94c8f63f420c55cd1916983
laurenz <laurenztino@yahoo.com>
2016-01-29 00:47:43 +0800

    [A] Added Quiver staging page

---
+++ quiver/js/laurenz.js
@@ -0,0 +1,27 @@
+/*
+    AUTHOR: LAURENZ (laurenz@outlook.ph)
+*/
+
+$(document).ready(function(){ 
+    var bodyheight = $(window).height();
+    $(".windowheight").css('min-height', bodyheight);
+    $(window).resize(function(){
+    var bodyheight = $(window).height();
+    $(".windowheight").css('min-height', bodyheight);
+    }); 
+});
+
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('.nav');
+   var offset = startchange.offset();
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('.nav').css('background-color', 
+                        'rgb(10, 27, 32)');
+       } else {
+          $('.nav').css('background-color', 'transparent');
+       }
+   });
+});
