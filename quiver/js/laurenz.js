b300c3e0d1a4eb9a94a42c97a0a934687edf5954
laurenz <laurenztino@yahoo.com>
2016-02-11 04:33:18 +0800

    [A] Added temp html filecalled trashMe

--- quiver/js/laurenz.js
+++ quiver/js/laurenz.js
@@ -2,14 +2,21 @@
     AUTHOR: LAURENZ (laurenz@outlook.ph)
 */
 
-$(document).ready(function(){ 
-    var bodyheight = $(window).height();
-    $(".windowheight").css('min-height', bodyheight);
-    $(window).resize(function(){
-    var bodyheight = $(window).height();
-    $(".windowheight").css('min-height', bodyheight);
-    }); 
-});
+$(document).ready(
+    function()
+    { 
+        var bodyheight = $(window).height();        
+        $(".windowheight").css('max-height', bodyheight);
+        
+        $(window).resize(
+            function()
+            {
+                var bodyheight = $(window).height();
+                $(".windowheight").css('max-height', bodyheight);
+            }
+        ); 
+    }
+);
 
 /*
 $(document).ready(function(){       
